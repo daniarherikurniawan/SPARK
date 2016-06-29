15c8b03b9ff3344227cead9cc53690b9e538f10a
daniarherikurniawan <ddhhkk2@gmail.com>
2016-06-30 02:34:35 +0700

    spark source code

---
+++ spark-1.6.1/conf/spark-env.sh
@@ -0,0 +1,10 @@
+export SPARK_WORKER_INSTANCES=2
+SPARK_MEM=${SPARK_MEM:-5120m}
+export SPARK_MEM
+export SPARK_PREPEND_CLASSES=true
+# export SPARK_MASTER_IP='192.168.1.8'
+
+# Set JAVA_OPTS to be able to load native libraries and to set heap size
+JAVA_OPTS="$OUR_JAVA_OPTS"
+JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$SPARK_LIBRARY_PATH"
+JAVA_OPTS="$JAVA_OPTS -Xms$SPARK_MEM -Xmx$SPARK_MEM"
---
+++ spark-1.6.1/work/app-20160617144937-0000/1/sort.py
@@ -0,0 +1,18 @@
+from pyspark import SparkContext,SparkConf
+import shutil,os
+
+# non-spark code
+if os.path.exists("/tmp/sort_result"):
+    shutil.rmtree("/tmp/sort_result")
+
+conf = SparkConf().setAppName("Sort5").setMaster("spark://192.168.43.28:7077").set("spark.eventLog.enabled","true").set("spark.eventLog.dir","../generated_driver_log/").set("spark.speculation","true")
+
+sc = SparkContext(conf=conf)
+text_file = sc.textFile("../generated_file/list_int",10)
+counts = text_file.map(lambda a : (int(a),a)).sortByKey("true")
+
+#print counts.collect()
+counts.saveAsTextFile("../generated_file/result_py")
+
+
+
