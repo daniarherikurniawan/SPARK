2b06e36235f900ec71422c0df48dbe81053becd6
daniarherikurniawan <ddhhkk2@gmail.com>
2016-06-30 02:32:46 +0700

    first commit

---
+++ res-generator/generate_list_int.py
@@ -0,0 +1,24 @@
+print  "Start Batch Operation!"
+
+import os
+import random
+
+os.chdir('../generated_file/')
+
+fo = open("list_int_test", 'w')
+
+num = 0
+
+for num in range(0,5):
+	print "generating iteration "+str(num)
+	array = random.sample(range(1, 10000000), 3000000)
+	random.shuffle(array)
+
+	# fo.write(str(array))
+	for int in array:
+	   fo.write(str(int)+'\n')
+	num += 1;
+fo.close()
+
+print "Finish generating data to the file!"
+
