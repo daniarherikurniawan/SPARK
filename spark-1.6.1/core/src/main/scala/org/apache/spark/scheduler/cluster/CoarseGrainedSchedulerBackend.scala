56feff50d1a415d13e2b71c818f5339e7a89a702
daniarherikurniawan <ddhhkk2@gmail.com>
2016-07-23 21:17:05 +0700

    edit the needed core

--- spark-1.6.1/core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
+++ spark-1.6.1/core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
@@ -202,7 +202,7 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val rpcEnv: Rp
       val workOffers = activeExecutors.map { case (id, executorData) =>
         new WorkerOffer(id, executorData.executorHost, executorData.freeCores)
       }.toSeq
-//      logInfo("daniarr -- makeOffers on all executors " + activeExecutors)
+      logInfo("DANIAR -- MAKE OFFERS TO all SLAVES" + activeExecutors)
       launchTasks(scheduler.resourceOffers(workOffers))
     }
 
