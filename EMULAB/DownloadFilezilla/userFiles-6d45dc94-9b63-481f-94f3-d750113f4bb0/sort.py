c081fc629f0f10a32f2386ce31a07c312f7b65b3
daniarherikurniawan <ddhhkk2@gmail.com>
2016-08-02 17:34:16 +0700

    edit threshold

---
+++ EMULAB/DownloadFilezilla/userFiles-6d45dc94-9b63-481f-94f3-d750113f4bb0/sort.py
@@ -0,0 +1,16 @@
+from pyspark import SparkContext,SparkConf
+import shutil,os
+
+# non-spark code
+if os.path.exists("../generated_file/result_py"):
+    shutil.rmtree("../generated_file/result_py")
+#some of the configuration is set through declaration at spark-defaults.conf
+conf = SparkConf().setAppName("Sorting").setMaster("spark://n1.testspark.cs331-uc.emulab.net:7077")
+
+sc = SparkContext(conf=conf)
+text_file = sc.textFile("../generated_file/list_int",1)
+counts = text_file.map(lambda a : (int(a),a)).sortByKey("true")
+
+#print counts.collect()
+counts.saveAsTextFile("../generated_file/result_py")
+
