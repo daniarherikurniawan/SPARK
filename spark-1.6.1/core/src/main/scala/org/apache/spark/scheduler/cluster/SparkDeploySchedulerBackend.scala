f77894eef3e8fc6a85ef7f471c4c90b7d4e3c699
daniarherikurniawan <ddhhkk2@gmail.com>
2016-08-05 23:44:49 +0700

    update number minimum cores

--- spark-1.6.1/core/src/main/scala/org/apache/spark/scheduler/cluster/SparkDeploySchedulerBackend.scala
+++ spark-1.6.1/core/src/main/scala/org/apache/spark/scheduler/cluster/SparkDeploySchedulerBackend.scala
@@ -148,7 +148,7 @@ private[spark] class SparkDeploySchedulerBackend(
     logInfo("DANIAR DO THE HACK in SparkDeploySchedulerBackend.scala")
 
 //    totalCoreCount.get() >= totalExpectedCores * minRegisteredRatio
-    totalCoreCount.get() >= 8
+    totalCoreCount.get() >= 8*4
   }
 
   override def applicationId(): String =
