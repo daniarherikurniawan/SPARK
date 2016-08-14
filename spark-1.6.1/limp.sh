c6c317a124a0214a5ba40c131624a3cb75644207
daniarherikurniawan <ddhhkk2@gmail.com>
2016-08-14 19:07:10 +0700

    update limp.sh

--- spark-1.6.1/limp.sh
+++ spark-1.6.1/limp.sh
@@ -1,33 +1,23 @@
-DOWNLINK=5000
-UPLINK=5000
+DOWNLINK=100
+UPLINK=100
 DEV=eth3
 
 # clean existing down- and uplink qdiscs, hide errors
-tc qdisc del dev $DEV root    
+tc qdisc del dev $DEV root   
 tc qdisc del dev $DEV ingress 
 
 ###### uplink
 
 # install root CBQ
+tc qdisc add dev $DEV  root handle 1: cbq \
+avpkt 1000 bandwidth 2mbit
 
-# tc qdisc add dev $DEV root handle 1: cbq avpkt 1000 bandwidth 150mbit 
+tc class add dev $DEV  parent 1: classid 1:1 cbq \
+rate 700kbit allot 1500 prio 5 bounded isolated
 
-# shape everything at $UPLINK speed - this prevents huge queues in your
-# DSL modem which destroy latency:
-# main class
+tc filter add dev $DEV  parent 1: protocol ip \
+prio 16 u32 match ip dst 0.0.0.0/0 flowid 1:1
 
-########## downlink #############
-# slow downloads down to somewhat less than the real speed  to prevent 
-# queuing at our ISP. Tune to see how high you can set it.
-# ISPs tend to have *huge* queues to make sure big downloads are fast
-#
-# attach ingress policer:
+tc filter add dev $DEV  parent 1: protocol ip \
+prio 16 u32 match ip src 0.0.0.0/0 flowid 1:1
 
-tc qdisc add dev $DEV handle ffff: ingress
-
-# filter *everything* to it (0.0.0.0/0), drop everything that's
-# coming in too fast:
-
-tc filter add dev $DEV parent ffff: protocol ip prio 5 u32 match ip src \
-   0.0.0.0/0 police  rate 320000kbps burst 10240  mpu 0 action drop
-   # /continue 
