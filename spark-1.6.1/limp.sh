ea09edee60449c7f873b8c26e59da3f40d77e11d
daniarherikurniawan <ddhhkk2@gmail.com>
2016-08-14 14:38:46 +0700

    update limp.sh

--- spark-1.6.1/limp.sh
+++ spark-1.6.1/limp.sh
@@ -1,16 +1,22 @@
-DOWNLINK=10
-UPLINK=10
-DEV=eth0
-
 # clean existing down- and uplink qdiscs, hide errors
-tc qdisc del dev $DEV root    2> /dev/null > /dev/null
-tc qdisc del dev $DEV ingress 2> /dev/null > /dev/null
+tc qdisc del dev eth0 root    
+tc qdisc del dev eth0 ingress 
 
 ###### uplink
 
-# install root CBQ
-tc qdisc del dev $DEV root
-tc qdisc add dev $DEV root handle 1: cbq avpkt 1000 bandwidth 100mbit
-tc class add dev $DEV parent 1: classid 1:1 cbq rate 256kbit allot 1500 prio 5 bounded isolated
-tc filter add dev $DEV parent 1: protocol ip prio 16 u32 match ip dst 10.1.1.4 flowid 1:1
-tc qdisc add dev $DEV parent 1:1 sfq perturb 10
+ # implicitly sets up class 1:1
+    tc qdisc add dev eth0 root handle 1 plr 0.05
+
+    # attach to class 1:1 and tell the module the default place to send
+    # traffic is to class 2:1 (could attach filters to discriminate)
+    tc qdisc add dev eth0 parent 1:1 handle 2 htb default 1
+
+    # class 2:1 does the actual limiting
+    tc class add dev eth0 parent 2 classid 2:1 htb rate 50Mbit ceil 50Mbit
+
+    # attach to class 2:1, also implicitly creates class 3:1, and attaches
+    # a FIFO queue to it.
+    tc qdisc add dev eth0 parent 2:1 handle 3 delay usecs 20000
+
+
+ iptables -t mangle -A PREROUTING -i eth0 -j IMQ --todev 0
