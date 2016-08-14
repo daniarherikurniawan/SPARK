b3b2cdc0ed394c4a24e9a6b283861a8312f96cf6
daniarherikurniawan <ddhhkk2@gmail.com>
2016-08-14 14:31:23 +0700

    update limp.sh

--- spark-1.6.1/limp.sh
+++ spark-1.6.1/limp.sh
@@ -12,5 +12,5 @@ tc qdisc del dev $DEV ingress 2> /dev/null > /dev/null
 tc qdisc del dev $DEV root
 tc qdisc add dev $DEV root handle 1: cbq avpkt 1000 bandwidth 100mbit
 tc class add dev $DEV parent 1: classid 1:1 cbq rate 256kbit allot 1500 prio 5 bounded isolated
-tc filter add dev $DEV parent 1: protocol ip prio 16 u32 match ip dst 10.1.1.* flowid 1:1
+tc filter add dev $DEV parent 1: protocol ip prio 16 u32 match ip dst 10.1.1.4 flowid 1:1
 tc qdisc add dev $DEV parent 1:1 sfq perturb 10
