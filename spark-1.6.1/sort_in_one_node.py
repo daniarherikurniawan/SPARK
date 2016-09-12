f239d92da7a4009d5f9e009241f56ecda2ff70ae
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-13 04:52:29 +0700

    update conf file

--- spark-1.6.1/sort_in_one_node.py
+++ spark-1.6.1/sort_in_one_node.py
@@ -12,3 +12,4 @@ text_file = sc.textFile("../generated_file/list_int")
 counts = text_file.map(lambda a : (int(a),a)).sortByKey("true")
 counts.saveAsTextFile("../generated_file/result_py")
 
+# ./bin/spark-submit sort_in_one_node.py --master spark://node-1.testspark.cs331-uc.emulab.net:7077 --deploy-mode cluster --num-executors 1
