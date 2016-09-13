c8df38002fcdb43beaa792b995fc1f305fcd5ea4
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-13 10:41:23 +0700

    update spark env

--- spark-1.6.1/conf/spark-env.sh
+++ spark-1.6.1/conf/spark-env.sh
@@ -3,10 +3,11 @@
 export SPARK_PREPEND_CLASSES=true
 export SPARK_WORKER_INSTANCES=2
 
-export SPARK_LOCAL_IP=155.98.36.0
+export SPARK_LOCAL_IP=10.1.1.2
 
 # Set JAVA_OPTS to be able to load native libraries and to set heap size
 JAVA_OPTS="$OUR_JAVA_OPTS"
 JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$SPARK_LIBRARY_PATH"
 JAVA_OPTS="$JAVA_OPTS -Xms$SPARK_MEM -Xmx$SPARK_MEM"
 
+# setenv SPARK_LOCAL_IP 10.1.1.2
