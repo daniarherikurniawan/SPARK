f5fee09158722cd62c4fd14bae8acaa0180f619a
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-14 12:20:57 +0700

    update topology

--- spark-1.6.1/sort_in_node.py
+++ spark-1.6.1/sort_in_node.py
@@ -5,7 +5,7 @@ import shutil,os
 if os.path.exists("../generated_file/result_py"):
     shutil.rmtree("../generated_file/result_py")
 #some of the configuration is set through declaration at spark-defaults.conf
-conf = SparkConf().setAppName("Sorting").setMaster("spark://node-5.testspark.cs331-uc.emulab.net:7077")
+conf = SparkConf().setAppName("Sorting").setMaster("spark://node-5.sparknewtopology.cs331-uc.emulab.net:7077")
 
 sc = SparkContext(conf=conf)
 text_file = sc.textFile("../generated_file/list_int",2)
