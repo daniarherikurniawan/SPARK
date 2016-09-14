6ae2a7f9a9210d0ecc73159ce04e14f3f889e3ed
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-14 12:17:06 +0700

    update topology

--- spark-1.6.1/conf/spark-env.sh
+++ spark-1.6.1/conf/spark-env.sh
@@ -1,9 +1,9 @@
 # SPARK_MEM=${SPARK_MEM:-5120m}
 # export SPARK_MEM
 export SPARK_PREPEND_CLASSES=true
-export SPARK_WORKER_INSTANCES=2
+# export SPARK_WORKER_INSTANCES=2
 
-export SPARK_LOCAL_IP=pc414.emulab.net
+# export SPARK_LOCAL_IP=pc414.emulab.net
 
 # Set JAVA_OPTS to be able to load native libraries and to set heap size
 JAVA_OPTS="$OUR_JAVA_OPTS"
