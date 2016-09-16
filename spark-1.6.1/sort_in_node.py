52336352b2f45cd04c97b480101e6ff037dbc5bb
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-16 10:45:09 +0700

    update apps

--- spark-1.6.1/sort_in_node.py
+++ spark-1.6.1/sort_in_node.py
@@ -13,19 +13,24 @@ conf = SparkConf().setAppName("Sorting").setMaster("spark://node-1.testspark.cs3
 # conf = SparkConf().setAppName("Sorting").setMaster("spark://daniar-X450JF:7077")
 
 sc = SparkContext(conf=conf)
-text_file = sc.textFile("../generated_file/list_int",2)
-sorted_array = text_file.map(lambda a : (int(a),a)).sortByKey("true")
+
+lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+sorted_array = lines.map(lambda a : (int(a),a)).sortByKey("true")
+
+
+# text_file = sc.textFile("../generated_file/list_int",2)
+# sorted_array = text_file.map(lambda a : (int(a),a)).sortByKey("true")
 
 print("start the delay. Inject TC!!")
 time.sleep(30)
 print("end the delay")
 
-# output = sorted_array.collect()
-# f = open('workfile', 'w')
-# for (num, unitcount) in output:
-# 	f.write(str(num)+"\n")
-#     # print(num)
-# f.closed
-sorted_array.saveAsTextFile("/proj/cs331-uc/daniar/result_py")
+output = sorted_array.collect()
+f = open('workfile', 'w')
+for (num, unitcount) in output:
+	f.write(str(num)+"\n")
+    # print(num)
+f.closed
+# sorted_array.saveAsTextFile("/proj/cs331-uc/daniar/result_py")
 
 # ./bin/spark-submit sort_in_node.py --master spark://daniar-X450JF:7077 --deploy-mode cluster --num-executors 4
