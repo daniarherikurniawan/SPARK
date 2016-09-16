7fa63649fff6c864625310a8ee2fa67c714517c8
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-16 11:05:31 +0700

    update apps

--- spark-1.6.1/group_by_app.py
+++ spark-1.6.1/group_by_app.py
@@ -4,7 +4,6 @@ import shutil,os, sys, time
 conf = SparkConf().setAppName("Sorting").setMaster("spark://node-1.testspark.cs331-uc.emulab.net:7077")
 
 sc = SparkContext(conf=conf)
-rdd = sc.parallelize([1, 1, 2, 3, 5, 8])
-result = rdd.groupBy(lambda x: x % 2).collect()
-
-
+lines = sc.textFile("../generated_file/list_int",2)
+result1 = lines.map(lambda a : (int(a),a))
+result2 = result1.groupByKey()
