93bb106fd058dfd317965ac5e654d9e0b4a72a49
daniarherikurniawan <ddhhkk2@gmail.com>
2016-09-16 10:56:18 +0700

    update apps

--- spark-1.6.1/group_by_app.py
+++ spark-1.6.1/group_by_app.py
@@ -4,15 +4,7 @@ import shutil,os, sys, time
 conf = SparkConf().setAppName("Sorting").setMaster("spark://node-1.testspark.cs331-uc.emulab.net:7077")
 
 sc = SparkContext(conf=conf)
-rdd = sc.textFile("../generated_file/list_int",2)
-words = rdd.toArray
-
-wordPairsRDD = sc.parallelize(words).map(word => (word, 1))
-
-
-wordCountsWithGroup = wordPairsRDD
-  .groupByKey()
-  .map(t => (t._1, t._2.sum))
-  .collect()
+rdd = sc.parallelize([1, 1, 2, 3, 5, 8])
+result = rdd.groupBy(lambda x: x % 2).collect()
 
 
